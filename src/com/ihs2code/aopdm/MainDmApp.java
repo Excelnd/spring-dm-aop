d4d71c8e1ff3c6fdd6bc1b4b52664cab569a5e61
Admin i <ihsanmpm@gmail.com>
2021-07-10 20:23:06 +0530

    add*

--- src/com/ihs2code/aopdm/MainDmApp.java
+++ src/com/ihs2code/aopdm/MainDmApp.java
@@ -24,7 +24,7 @@ public class MainDmApp {
 		theAccountDAO.addAccount();
 		
 		// call the membership business method
-		theMembershipDAO.addAccount();
+		theMembershipDAO.addSillyMember();
 		
 		// close the context
 		context.close();
