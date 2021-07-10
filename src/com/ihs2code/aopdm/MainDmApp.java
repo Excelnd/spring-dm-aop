b13b74e461d62e02093cc77f95bf3b0517b617b4
Admin i <ihsanmpm@gmail.com>
2021-07-10 18:51:41 +0530

    AccountDAO bean

---
+++ src/com/ihs2code/aopdm/MainDmApp.java
@@ -0,0 +1,26 @@
+package com.ihs2code.aopdm;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.ihs2code.aopdm.dao.AccountDAO;
+
+public class MainDmApp {
+
+	public static void main(String[] args) {
+		
+		// read spring config java class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(DmConfig.class);
+		
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		// call the business method
+		theAccountDAO.addAccount();
+		
+		// close the context
+		context.close();
+
+	}
+
+}
