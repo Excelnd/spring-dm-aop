0cac4eec40bfb288b64a6948745e83fcaddc0d3c
Admin i <ihsanmpm@gmail.com>
2021-07-11 22:47:30 +0530

    aspect @AfterReturning - modifying data

--- src/com/ihs2code/aopdm/aspect/MyDmLoggingAspect.java
+++ src/com/ihs2code/aopdm/aspect/MyDmLoggingAspect.java
@@ -1,6 +1,9 @@
 package com.ihs2code.aopdm.aspect;
 
+import java.util.List;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -14,6 +17,43 @@ import com.ihs2code.aopdm.Account;
 @Order(2)
 public class MyDmLoggingAspect {
 	
+	// add a new device for @AfterReturning on the findAccounts method
+	@AfterReturning(
+			pointcut="execution(* com.ihs2code.aopdm.dao.AccountDAO.findAccounts(..))",
+			returning="result")
+	public void afterReturningFindAccountsAdvice(
+					JoinPoint theJoinPoint, List<Account> result) {
+		
+		// print out which method are advising on
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n==========>>>>> Executing @AfterReturning on method: " + method);
+		
+		// print out the results of the method call
+		System.out.println("\n==========>>>>> result is: " + result);
+		
+		// post-process the data ... modification
+		
+		// convert the account names to uppercase
+		converAccountNamesToUpperCase(result);
+		
+		System.out.println("\n=======>>>>> result is: " + result);
+		
+	}
+		
+	private void converAccountNamesToUpperCase(List<Account> result) {
+		// loop through accounts
+		
+		for (Account tempAccount: result) {
+			
+			// get uppercase name
+			String theUpperName = tempAccount.getName().toUpperCase();
+			
+			// update the name on the account
+			tempAccount.setName(theUpperName);
+		}
+		
+	}
+
 	@Before("com.ihs2code.aopdm.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
 		
